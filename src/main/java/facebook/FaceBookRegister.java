9b70ddbbd9114a2249057f190a857fe97e5b719b
srikanth <srikanth@192.168.0.9>
2017-06-29 09:03:44 +0530

    name validaiton done.

--- src/main/java/facebook/FaceBookRegister.java
+++ src/main/java/facebook/FaceBookRegister.java
@@ -34,6 +34,7 @@ public class FaceBookRegister {
 	
 	public String loginFlow(){
 		System.out.println("Login flow implemented");
+		System.out.println("Name validations");
 		return "showProfiles";
 	}
 }
